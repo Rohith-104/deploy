3baf3b59c4efa94f18c56f556cdfbd9e5ac66b28
Rohith-104 <rohithecevlsi@gmail.com>
2026-03-24 22:21:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// DO NOT SHARE YOUR SUPABASE_KEY WITH OTHERS.
+// You can find these in your Supabase Project Settings > API
+const SUPABASE_URL = "https://yfffgvdewgisgbvdmbid.supabase.co";
+const SUPABASE_KEY = "sb_publishable_LR4LYxerkOGSV4wXVxjbvQ_7n52xeFO"; 
